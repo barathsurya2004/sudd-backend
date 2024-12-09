cdd2f778f325f511a11617790ec4ee4537bb9dba
Zeus Crusher <barathsurya2004@gmail.com>
2024-12-09 13:50:18 +0530

    added vercel

--- gdriveroute.js
+++ gdriveroute.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 const router = express.Router();
 
-router.post("/upload", upload.single("image"), async (req, res) => {
+router.post("/temp", upload.single("image"), async (req, res) => {
     try {
       const { title, text } = req.body; 
       const imageFile = req.file;
